eb3281957309f7ade528383a42414a5665c5a648
fjcorugda-del <fjcorugda@gmail.com>
2026-05-13 18:48:52 +0800

    Update package

--- app/src/routes/AppRoutes.tsx
+++ app/src/routes/AppRoutes.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
-import BlogPage from '../pages/BlogPage';
-import AboutPage from '../pages/AboutPage';
-import LoginPage from '../pages/LoginPage';
-import ArticleDetailsPage from '../pages/ArticleDetailsPage';
+import HomePage from '../pages/HomePage.js';
+import BlogPage from '../pages/BlogPage.js';
+import AboutPage from '../pages/AboutPage.js';
+import LoginPage from '../pages/LoginPage.js';
+import ArticleDetailsPage from '../pages/ArticleDetailsPage.js';
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/blog" element={<BlogPage />} />
-      <Route path="/blog/:id" element={<ArticleDetailsPage id={0} title={''} description={''}/>} />
+      <Route path="/blog/:id" element={<ArticleDetailsPage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
